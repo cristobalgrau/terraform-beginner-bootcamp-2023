ac1ec9a854c32b3c382177ab685df8aa7675d6f6
Cristobal Grau <cristobalgrau@gmail.com>
2023-09-25 04:56:30 +0000

    #19 changed root module structure and add tag to bucket

--- main.tf
+++ main.tf
@@ -1,38 +1,4 @@
-terraform {
-  # backend "remote" {
-  #   hostname = "app.terraform.io"
-  #   organization = "GrauTerraformLab"
-
-  # workspaces {
-  #   name = "terra-house-lab"
-  #   }
-  # }
-  cloud {
-    organization = "GrauTerraformLab"
-
-    workspaces {
-      name = "terra-house-lab"
-    }
-  }
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-
-provider "aws" {
-  # Configuration options
-}
-provider "random" {
-  # Configuration options
-}
-
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
   lower = true
   upper = false 
@@ -40,10 +6,13 @@ resource "random_string" "bucket_name" {
   special = false
 }
 
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 resource "aws_s3_bucket" "example" {
+  # Bucket Naming Rules
+  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
   bucket = random_string.bucket_name.result
-}
 
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
+  # tags = {
+  #   UserUuid = var.user_uuid
+  # }
 }
