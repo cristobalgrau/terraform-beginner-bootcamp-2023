790eb41d35869b563096dfdb956c425a999ab44b
Cristobal Grau <cristobalgrau@gmail.com>
2023-09-30 16:02:36 +0000

    #29 creating content type control - part 1

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -29,6 +29,9 @@ resource "aws_s3_object" "index_html" {
   content_type = "text/html"
   
   etag = filemd5(var.index_html_filepath)
+  lifecycle {
+    ignore_changes = [ etag ]
+  }
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
@@ -36,8 +39,12 @@ resource "aws_s3_object" "error_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "error.html"
   source = var.error_html_filepath
+  content_type = "text/html"
   
   etag = filemd5(var.error_html_filepath)
+  lifecycle {
+    ignore_changes = [ etag ]
+  }
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
