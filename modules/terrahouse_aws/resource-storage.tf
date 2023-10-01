d7eafc5f5c6b2e2106dbc7cfbf95384dabfe5907
Cristobal Grau <cristobalgrau@gmail.com>
2023-10-01 01:49:22 +0000

    #29 creating content type control - part 2

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -30,6 +30,7 @@ resource "aws_s3_object" "index_html" {
   
   etag = filemd5(var.index_html_filepath)
   lifecycle {
+    replace_triggered_by = [ terraform_data.content_version.output ]
     ignore_changes = [ etag ]
   }
 }
@@ -42,9 +43,9 @@ resource "aws_s3_object" "error_html" {
   content_type = "text/html"
   
   etag = filemd5(var.error_html_filepath)
-  lifecycle {
-    ignore_changes = [ etag ]
-  }
+  # lifecycle {
+  #   ignore_changes = [ etag ]
+  # }
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
@@ -68,4 +69,9 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
       }
     },
   })
+}
+
+
+resource "terraform_data" "content_version" {
+  input = var.content_version
 }
